43a447d0573470efd4acbb97c711e247b120d521
Lucas Müllner <lucas.muellner@external.erstegroup.com>
2025-11-25 12:14:14 +0100

    upgrades Spring Boot to v4.0.0

--- src/main/java/com/teatown/software/springboot/demo/DemoApplication.java
+++ src/main/java/com/teatown/software/springboot/demo/DemoApplication.java
@@ -2,14 +2,14 @@ package com.teatown.software.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
